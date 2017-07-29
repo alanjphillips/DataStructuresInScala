6e5a2ef55bd6651316a7d7ff7f8008c40666c8f9
alanp <alanphillips78@gmail.com>
2017-07-29 23:23:31 +0100

    Further LinkedList implementation

--- src/main/scala/linkedlist/List.scala
+++ src/main/scala/linkedlist/List.scala
@@ -2,17 +2,23 @@ package linkedlist
 
 import scala.annotation.tailrec
 
-case object Empty extends LinkedList[Nothing]
+case object Empty extends LinkedList[Nothing] {
+  val size = 0
+}
 
-case class Node[A](element: A, tail: LinkedList[A]) extends LinkedList[A]
+case class Node[A](element: A, tail: LinkedList[A]) extends LinkedList[A] {
+  val size = 1 + tail.size
+}
 
 trait LinkedList[+A] {
+
+  def size: Int
 
   def prepend[B >: A](element: B): LinkedList[B] = Node(element, this)
 
   @tailrec
   final def foldLeft[B](acc: B)(f: (B, A) => B): B = this match {
-    case Empty               => acc
+    case Empty => acc
     case Node(element, next) => next.foldLeft(f(acc, element))(f)
   }
 
@@ -20,7 +26,7 @@ trait LinkedList[+A] {
     reverse.foldLeft(acc)(f)
 
   def reverse: LinkedList[A] =
-    foldLeft(LinkedList[A]()){
+    foldLeft(LinkedList[A]()) {
       (acc, next) => {
         if (next != Empty)
           Node(next, acc)
@@ -48,6 +54,28 @@ trait LinkedList[+A] {
           acc
       }
     }
+
+  def findByPosition(position: Int): Option[A] = {
+    @tailrec
+    def inner(curr: Int, pos: Int, node: LinkedList[A]): Option[A] = node match {
+      case Empty => None
+      case Node(element, next) => {
+        if (curr == pos) Some(element)
+        else inner(curr + 1, pos, next)
+      }
+    }
+    inner(1, position, this)
+  }
+
+  def removeByPosition(position: Int): LinkedList[A] =
+    foldLeft((1, LinkedList[A]())) {
+      (acc, next) => {
+        if (position == acc._1)
+          (acc._1 + 1, acc._2)
+        else
+          (acc._1 + 1, Node(next, acc._2))
+      }
+    }._2.reverse
 
 }
 
--- src/test/scala/linkedlist/ListSpec.scala
+++ src/test/scala/linkedlist/ListSpec.scala
@@ -65,6 +65,32 @@ class ListSpec extends WordSpec with Matchers {
       val listMulti = LinkedList(22, 33, 44, 55)
       listMulti.remove(44) shouldBe Node(22, Node(33, Node(55, Empty)))
     }
+
+    "find node by position in a LinkedList" in {
+      val listMulti = LinkedList(22, 33, 44, 55)
+      listMulti.findByPosition(0) shouldBe None
+      listMulti.findByPosition(1) shouldBe Some(22)
+      listMulti.findByPosition(2) shouldBe Some(33)
+      listMulti.findByPosition(3) shouldBe Some(44)
+      listMulti.findByPosition(4) shouldBe Some(55)
+      listMulti.findByPosition(5) shouldBe None
+    }
+
+    "remove node by position from a LinkedList" in {
+      val listMulti = LinkedList(22, 33, 44, 55)
+      listMulti.removeByPosition(0) shouldBe Node(22, Node(33, Node(44, Node(55, Empty))))
+      listMulti.removeByPosition(1) shouldBe Node(33, Node(44, Node(55, Empty)))
+      listMulti.removeByPosition(2) shouldBe Node(22, Node(44, Node(55, Empty)))
+      listMulti.removeByPosition(3) shouldBe Node(22, Node(33, Node(55, Empty)))
+      listMulti.removeByPosition(4) shouldBe Node(22, Node(33, Node(44, Empty)))
+      listMulti.removeByPosition(5) shouldBe Node(22, Node(33, Node(44, Node(55, Empty))))
+    }
+
+    "return size of a LinkedList" in {
+      LinkedList().size shouldBe 0
+      LinkedList(22).size shouldBe 1
+      LinkedList(22, 33, 44, 55).size shouldBe 4
+    }
   }
 
 }
