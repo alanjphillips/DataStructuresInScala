cd124b7aa2aa794d0b8ddcbb8598b810c912574f
alanp <alanphillips78@gmail.com>
2017-08-06 18:55:44 +0100

    Search tree method

--- src/main/scala/tree/Tree.scala
+++ src/main/scala/tree/Tree.scala
@@ -13,6 +13,13 @@ trait Tree[+A] {
     case Node(element, _, _) if (cmp.equiv(elem, element))     => this
   }
 
+  def search[B >: A](elem: B)(implicit cmp: Ordering[B]): Boolean = this match {
+    case Empty => false
+    case Node(element, _, _) if (cmp.equiv(elem, element)) => true
+    case Node(element, left, _) if (cmp.lt(elem, element)) => left.search(elem)
+    case Node(element, _, right) if (cmp.gt(elem, element)) => right.search(elem)
+  }
+
   def delete[B >: A](elem: B): Tree[B] = Empty
 
 }
--- src/test/scala/tree/TreeSpec.scala
+++ src/test/scala/tree/TreeSpec.scala
@@ -12,9 +12,19 @@ class TreeSpec extends WordSpec with Matchers {
       tree shouldBe Empty
     }
 
-    "create an Binary Search Tree and insert 3 elements" in {
+    "create a Binary Search Tree and insert 3 elements" in {
       val tree = Tree[Int].insert(5).insert(3).insert(7)
       tree shouldBe tree3Elements
+    }
+
+    "search a Binary Search Tree" in {
+      val tree = Tree[Int].insert(5).insert(3).insert(7)
+      tree.search(5) shouldBe true
+      tree.search(3) shouldBe true
+      tree.search(7) shouldBe true
+      tree.search(2) shouldBe false
+      tree.search(4) shouldBe false
+      tree.search(8) shouldBe false
     }
 
   }
