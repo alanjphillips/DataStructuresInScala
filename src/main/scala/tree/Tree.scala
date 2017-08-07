f34b69dcbc99fbd4bcf0a269daf624dfd7246540
alanp <alanphillips78@gmail.com>
2017-08-07 18:23:23 +0100

    Delete element from Tree

--- src/main/scala/tree/Tree.scala
+++ src/main/scala/tree/Tree.scala
@@ -2,25 +2,40 @@ package tree
 
 case object Empty extends Tree[Nothing]
 
-case class Node[+A](element: A, left: Tree[A], right: Tree[A]) extends Tree[A]
+case class Node[+A <% Ordered[A]](element: A, left: Tree[A], right: Tree[A]) extends Tree[A]
 
-trait Tree[+A] {
+abstract class Tree[+A <% Ordered[A]] {
 
-  def insert[B >: A](elem: B)(implicit cmp: Ordering[B]): Tree[B] = this match {
+  def insert[B >: A <% Ordered[B]](elem: B): Tree[B] = this match {
     case Empty => Node(elem, Empty, Empty)
-    case Node(element, left, right) if (cmp.lt(elem, element)) => Node(element, left.insert(elem), right)
-    case Node(element, left, right) if (cmp.gt(elem, element)) => Node(element, left, right.insert(elem))
-    case Node(element, _, _) if (cmp.equiv(elem, element))     => this
+    case Node(element, left, right) if elem < element => Node(element, left.insert(elem), right)
+    case Node(element, left, right) if elem > element => Node(element, left, right.insert(elem))
+    case Node(element, _, _) if elem == element       => this
   }
 
-  def search[B >: A](elem: B)(implicit cmp: Ordering[B]): Boolean = this match {
+  def search[B >: A <% Ordered[B]](elem: B): Boolean = this match {
     case Empty => false
-    case Node(element, _, _) if (cmp.equiv(elem, element)) => true
-    case Node(element, left, _) if (cmp.lt(elem, element)) => left.search(elem)
-    case Node(element, _, right) if (cmp.gt(elem, element)) => right.search(elem)
+    case Node(element, _, _) if elem == element    => true
+    case Node(element, left, _) if elem < element  => left.search(elem)
+    case Node(element, _, right) if elem > element => right.search(elem)
   }
 
-  def delete[B >: A](elem: B): Tree[B] = Empty
+  def delete[B >: A <% Ordered[B]](elem: B): Tree[B] = this match {
+    case Empty => Empty
+    case Node(element, left, right) if elem < element      => Node(element, left.delete(elem), right)
+    case Node(element, left, right) if elem > element      => Node(element, left, right.delete(elem))
+    case Node(element, Empty, Empty) if elem == element    => Empty
+    case Node(element, left, Empty) if elem == element     => left
+    case Node(element, Empty, right) if elem == element    => right
+    case n @ Node(element, _, right) if elem == element =>
+      val replacement = leftMostChild(right)
+      Node(replacement.element, n.left, n.right.delete(replacement.element))
+  }
+
+  def leftMostChild[B >: A](tree: Tree[B]): Node[B] = tree match {
+    case n @ Node(_, Empty, _) => n
+    case Node(_, left, _)      => leftMostChild(left)
+  }
 
 }
 
--- src/test/scala/tree/TreeSpec.scala
+++ src/test/scala/tree/TreeSpec.scala
@@ -27,6 +27,21 @@ class TreeSpec extends WordSpec with Matchers {
       tree.search(8) shouldBe false
     }
 
+    "delete element with no left child from a Binary Search Tree" in {
+      val tree = Tree[Int].insert(5).insert(3).insert(7)
+      tree.delete(3) shouldBe tree2ElementsR
+    }
+
+    "delete element with no right child from a Binary Search Tree" in {
+      val tree = Tree[Int].insert(5).insert(3).insert(7)
+      tree.delete(7) shouldBe tree2ElementsL
+    }
+
+    "delete element with 2 children a Binary Search Tree" in {
+      val tree = treeBig.copy()
+      tree.delete(12) shouldBe treeBigMinus12
+    }
+
   }
 
 }
@@ -37,6 +52,48 @@ object TreeSpec {
     Node(5,
       left = Node(3, Empty, Empty),
       right = Node(7, Empty, Empty)
+    )
+
+  val tree2ElementsR =
+    Node(5,
+      left = Empty,
+      right = Node(7, Empty, Empty)
+    )
+
+  val tree2ElementsL =
+    Node(5,
+      left = Node(3, Empty, Empty),
+      right = Empty
+    )
+
+  val treeBig =
+    Node(5,
+      Node(2,
+        Node(-4 , Empty, Empty),
+        Node(3 , Empty, Empty)
+      ),
+      Node(12,
+        Node(9, Empty, Empty),
+        Node(21,
+          Node(19, Empty, Empty),
+          Node(25, Empty, Empty)
+        )
+      )
+    )
+
+  val treeBigMinus12 =
+    Node(5,
+      Node(2,
+        Node(-4 , Empty, Empty),
+        Node(3 , Empty, Empty)
+      ),
+      Node(19,
+        Node(9, Empty, Empty),
+        Node(21,
+          Empty,
+          Node(25, Empty, Empty)
+        )
+      )
     )
 
 }
